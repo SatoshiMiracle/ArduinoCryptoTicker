69f098042ad3a9f499344be38d5537481fe18b6f
SatoshiMiracle <72975337+SatoshiMiracle@users.noreply.github.com>
2021-05-14 16:34:54 +0300

    Upload Arduino Code

---
+++ Code/arduino.ino
@@ -0,0 +1,41 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+String inBytes;
+String previousBytes;
+
+void setup() {
+    lcd.init();
+    lcd.backlight();
+    pinMode(2, OUTPUT); //BUZZER
+    pinMode(4, OUTPUT); //GREEN LED
+    pinMode(5, OUTPUT); //RED LED
+    previousBytes = "0";
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (Serial.available() > 0) {
+        inBytes = Serial.readStringUntil('\n');
+        lcd.setCursor(2, 0);
+        lcd.print("BTC: $");
+        lcd.print(inBytes);
+
+        previousBytes = previousBytes.toInt();
+        inBytes = previousBytes.toInt();
+        
+        if (previousBytes > inBytes) {
+            digitalWrite(5, HIGH);
+            delay(1000);
+            digitalWrite(5, LOW);
+            lcd.clear();
+        }
+
+        else {
+            digitalWrite(4, HIGH);
+            delay(1000);
+            digitalWrite(4, LOW);
+            lcd.clear();
+        }
+
+        previousBytes = inBytes;
+}
